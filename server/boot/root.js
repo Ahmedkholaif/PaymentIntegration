e0ac3902914f3e4981136a519715d251b1b25e1d
ahmedkholaif <ahmed_kholaif@yahoo.com>
2019-08-07 17:35:57 +0200

    Fix typo

--- server/boot/root.js
+++ server/boot/root.js
@@ -6,7 +6,7 @@
 'use strict';
 
 module.exports = function(server) {
-  Install a `/` route that returns server status
+  // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/');
   router.get('/l',(re,res)=>{
